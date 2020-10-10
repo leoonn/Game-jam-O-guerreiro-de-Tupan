181f410b26c5fd3d6d0cd29afeea513ba8d47704
leoonn <flateia13@hotmail.com>
2020-10-10 19:37:34 -0300

    =enemy seguindo o player

---
+++ o guerreiro tupan/Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Animator anim;
+    public float speedEnemy = 3f;
+    public Transform target;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        FollowPlayer();
+    }
+
+    void FollowPlayer()
+    {
+        float step = speedEnemy * Time.deltaTime;
+        if (Vector3.Distance(transform.position, target.position) >= 0.001)
+        {
+
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step * Time.deltaTime);
+            transform.rotation = Quaternion.LookRotation(target.position * Time.deltaTime);
+        }
+    }
+}
--- o guerreiro tupan/Assets/Scripts/movementPlayer.cs
+++ o guerreiro tupan/Assets/Scripts/movementPlayer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class movementPlayer : MonoBehaviour
 {
-    public Transform cam;
+  
 
 
     public float speed = 5f;
