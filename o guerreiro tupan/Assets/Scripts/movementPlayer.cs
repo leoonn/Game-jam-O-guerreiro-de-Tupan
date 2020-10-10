6317d32c3706a5df7f54d8353a2f638daf57e687
leoonn <flateia13@hotmail.com>
2020-10-10 11:01:43 -0300

    subi uns negocio inutil

---
+++ o guerreiro tupan/Assets/Scripts/movementPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementPlayer : MonoBehaviour
+{
+    public float speed = 5f;
+
+    public Camera cam;
+    public float turnSmooth = 2f;
+    public float smoothroottime = 5f;
+
+    void Start()
+    {
+        speed = 5f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+    void Move()
+    {
+        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(speed * horizontal * Time.deltaTime, 0, speed * vertical);
+
+        if (direction.magnitude > 0)
+        {
+            float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
+            float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, angle, ref turnSmooth, smoothroottime);
+        }
+    }
+}
