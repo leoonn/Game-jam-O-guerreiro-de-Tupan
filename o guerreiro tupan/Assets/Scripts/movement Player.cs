6604966ea52540f2f35492876d94463b45ac7e52
leoonn <flateia13@hotmail.com>
2020-10-10 09:58:38 -0300

    movement metade

---
+++ o guerreiro tupan/Assets/Scripts/movement Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementPlayer : MonoBehaviour
+{
+    public float speed = 5f;
+
+    public Rigidbody rb;
+
+    public Camera cam;
+
+    Vector3 movement;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+    void Move()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+            
+    }
+}
